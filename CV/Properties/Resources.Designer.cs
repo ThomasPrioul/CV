72728d24ea2073af1393e453592dacbf086176ef
Thomas Prioul <thomas.prioul@outlook.com>
2018-08-23 20:32:55 +0200

    Add right pane

--- CV/Properties/Resources.Designer.cs
+++ CV/Properties/Resources.Designer.cs
@@ -61,6 +61,35 @@ namespace CV.Properties {
         }
         
         /// <summary>
+        ///   Recherche une chaîne localisée semblable à Logiciel &apos;valise&apos; pour les trains NAT et Régio2N :
+        /// - Détections d&apos;anomalies en temps réel, sur rame ou à distance
+        /// - Affichage graphique avec dessins et couleurs
+        /// - Simplifie la localisation des pannes pour les dépanneurs en dépôt
+        /// - Assiste le pôle d&apos;appui conduite (Gare du Nord)
+        ///
+        ///Banc de test réseaux pour le train Régio2N :
+        /// - Basé sur le banc NAT développé précédemment
+        /// - Simulation de 3x plus de réseaux et équipements
+        /// - Logiciel service web multiplateforme (Linux)
+        ///
+        ///Simulateur de mission pou [le reste de la chaîne a été tronqué]&quot;;.
+        /// </summary>
+        public static string ABCPortageJobDescription {
+            get {
+                return ResourceManager.GetString("ABCPortageJobDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Chef de projet informatique.
+        /// </summary>
+        public static string ABCPortageJobTitle {
+            get {
+                return ResourceManager.GetString("ABCPortageJobTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une chaîne localisée semblable à Centres d&apos;intérêt.
         /// </summary>
         public static string ActivitiesHeader {
@@ -89,11 +118,51 @@ namespace CV.Properties {
         }
         
         /// <summary>
+        ///   Recherche une chaîne localisée semblable à Programmation des microcontrôleurs, DSP, FPGA
+        ///Réseaux informatiques, linux et informatique embarquée
+        ///Applications pour PC et Linux embarqué
+        ///Modélisation UML, gestion de sources (git)
+        ///Support avec tickets, développement agile.
+        /// </summary>
+        public static string ComputingSkillsDescription {
+            get {
+                return ResourceManager.GetString("ComputingSkillsDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une chaîne localisée semblable à Contact.
         /// </summary>
         public static string ContactHeader {
             get {
                 return ResourceManager.GetString("ContactHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Formation.
+        /// </summary>
+        public static string EducationHeader {
+            get {
+                return ResourceManager.GetString("EducationHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Électronique.
+        /// </summary>
+        public static string ElectronicsHeader {
+            get {
+                return ResourceManager.GetString("ElectronicsHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Réalisation de schémas et routage de carte.
+        /// </summary>
+        public static string ElectronicsSkillsDescription {
+            get {
+                return ResourceManager.GetString("ElectronicsSkillsDescription", resourceCulture);
             }
         }
         
@@ -107,11 +176,39 @@ namespace CV.Properties {
         }
         
         /// <summary>
+        ///   Recherche une chaîne localisée semblable à Anglais niveau C1/C2
+        ///TOEIC : 990.
+        /// </summary>
+        public static string EnglishSkillLevel {
+            get {
+                return ResourceManager.GetString("EnglishSkillLevel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Expérience professionnelle.
+        /// </summary>
+        public static string ExperienceHeader {
+            get {
+                return ResourceManager.GetString("ExperienceHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une chaîne localisée semblable à Thomas Prioul.
         /// </summary>
         public static string FullName {
             get {
                 return ResourceManager.GetString("FullName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Informatique industrielle.
+        /// </summary>
+        public static string IndustrialComputingHeader {
+            get {
+                return ResourceManager.GetString("IndustrialComputingHeader", resourceCulture);
             }
         }
         
@@ -127,11 +224,38 @@ namespace CV.Properties {
         }
         
         /// <summary>
+        ///   Recherche une chaîne localisée semblable à IUT GEII de Tours.
+        /// </summary>
+        public static string IUTEducationDescription {
+            get {
+                return ResourceManager.GetString("IUTEducationDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à DUT GEII.
+        /// </summary>
+        public static string IUTEducationHeader {
+            get {
+                return ResourceManager.GetString("IUTEducationHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une chaîne localisée semblable à Ingénieur informatique industrielle.
         /// </summary>
         public static string JobTitle {
             get {
                 return ResourceManager.GetString("JobTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Langues.
+        /// </summary>
+        public static string LanguageSkillsHeader {
+            get {
+                return ResourceManager.GetString("LanguageSkillsHeader", resourceCulture);
             }
         }
         
@@ -174,6 +298,26 @@ namespace CV.Properties {
         }
         
         /// <summary>
+        ///   Recherche une chaîne localisée semblable à En alternance à Polytech Tours
+        ///Option véhicules autonomes
+        ///Major de la section informatique industrielle.
+        /// </summary>
+        public static string PolytechEducationDescription {
+            get {
+                return ResourceManager.GetString("PolytechEducationDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Ingénieur informatique industrielle.
+        /// </summary>
+        public static string PolytechEducationHeader {
+            get {
+                return ResourceManager.GetString("PolytechEducationHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Recherche une chaîne localisée semblable à Actuellement en poste en freelance pour la SNCF, je réalise des applications pour PC à destination de bancs de test ainsi qu&apos;à de l&apos;assistance au dépannage.
         ///
         ///Pour 2019 je cherche un poste plus tourné vers l&apos;électronique et le développement de produits à base de microcontrôleurs et/ou de cartes linux embarquées..
@@ -190,6 +334,39 @@ namespace CV.Properties {
         public static string ProfileHeader {
             get {
                 return ResourceManager.GetString("ProfileHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Compétences.
+        /// </summary>
+        public static string SkillsHeader {
+            get {
+                return ResourceManager.GetString("SkillsHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Banc de test réseaux pour le train NAT :
+        /// - Développement d&apos;architecture logicielle complexe
+        /// - Modification code source FPGA en C (processeur NIOS)
+        /// - Simulation en temps réel d&apos;équipements via un PC
+        /// - Test des équipements dans leur environnement nominal
+        /// - Réalisation de séquences (mise en service, arrêt d&apos;urgence...)
+        /// - Test des pièces en réparation.
+        /// </summary>
+        public static string SNCFJobDescription {
+            get {
+                return ResourceManager.GetString("SNCFJobDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une chaîne localisée semblable à Apprenti informatique industrielle.
+        /// </summary>
+        public static string SNCFJobTitle {
+            get {
+                return ResourceManager.GetString("SNCFJobTitle", resourceCulture);
             }
         }
     }
