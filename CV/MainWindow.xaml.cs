b94a367d0ac5a6e5efeffa28dfe28fb8d6faf9af
Thomas Prioul <thomas.prioul@outlook.com>
2018-08-23 17:37:43 +0200

    add print btn

--- CV/MainWindow.xaml.cs
+++ CV/MainWindow.xaml.cs
@@ -2,12 +2,26 @@
 {
     using System.Diagnostics;
     using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        void PrintCommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        void PrintCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var printDlg = new PrintDialog();
+            if (printDlg.ShowDialog() == true)
+                printDlg.PrintVisual(Content as Visual, "CV");
         }
 
         void UniformGrid_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
